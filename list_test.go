e8d2593600b12471f327189d55fd86e44e3abe4e
Harmen <harm@dds.nl>
2014-09-14 20:28:53 +0200

    RPOP and RPUSH

--- list_test.go
+++ list_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestList(t *testing.T) {
+func TestLPush(t *testing.T) {
 	testCommands(t,
 		succ("LPUSH", "l", "aap", "noot", "mies"),
 		succ("TYPE", "l"),
@@ -37,5 +37,32 @@ func TestList(t *testing.T) {
 		fail("LRANGE", "key", "2", "noint"),
 		fail("LPOP"),
 		fail("LPOP", "key", "args"),
+	)
+}
+
+func TestRPush(t *testing.T) {
+	testCommands(t,
+		succ("RPUSH", "l", "aap", "noot", "mies"),
+		succ("TYPE", "l"),
+		succ("RPUSH", "l", "more", "keys"),
+		succ("LRANGE", "l", "0", "-1"),
+		succ("LRANGE", "l", "0", "6"),
+		succ("LRANGE", "l", "2", "6"),
+		succ("RPOP", "l"),
+		succ("RPOP", "l"),
+		succ("RPOP", "l"),
+		succ("RPOP", "l"),
+		succ("RPOP", "l"),
+		succ("RPOP", "l"),
+		succ("EXISTS", "l"),
+		succ("RPOP", "nosuch"),
+
+		// failure cases
+		fail("RPUSH"),
+		fail("RPUSH", "l"),
+		succ("SET", "str", "I am a string"),
+		fail("RPUSH", "str", "noot", "mies"),
+		fail("RPOP"),
+		fail("RPOP", "key", "args"),
 	)
 }
