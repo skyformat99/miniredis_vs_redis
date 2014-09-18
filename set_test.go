d3e55f5874b6d4c5793d5801a2ac5c5dc55cdabd
Harmen <harm@dds.nl>
2014-09-18 21:09:48 +0200

    SCARD and SISMEMBER tests

--- set_test.go
+++ set_test.go
@@ -12,17 +12,31 @@ func TestSet(t *testing.T) {
 		succ("SADD", "s", "vuur", "noot"),
 		succ("TYPE", "s"),
 		succ("EXISTS", "s"),
+		succ("SCARD", "s"),
 		succSorted("SMEMBERS", "s"),
 		succSorted("SMEMBERS", "nosuch"),
+		succ("SISMEMBER", "s", "aap"),
+		succ("SISMEMBER", "s", "nosuch"),
+
+		succ("SCARD", "nosuch"),
+		succ("SISMEMBER", "nosuch", "nosuch"),
 
 		// failure cases
 		fail("SADD"),
 		fail("SADD", "s"),
 		fail("SMEMBERS"),
+		fail("SMEMBERS", "too", "many"),
+		fail("SCARD"),
+		fail("SCARD", "too", "many"),
+		fail("SISMEMBER"),
+		fail("SISMEMBER", "few"),
+		fail("SISMEMBER", "too", "many", "arguments"),
 		// Wrong type
 		succ("SET", "str", "I am a string"),
 		fail("SADD", "str", "noot", "mies"),
 		fail("SMEMBERS", "str"),
+		fail("SISMEMBER", "str", "noot"),
+		fail("SCARD", "str"),
 
 		// Move a set around
 		succ("RENAME", "s", "others"),
