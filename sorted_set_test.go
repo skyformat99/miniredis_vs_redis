63a2a50ec36409b5281f6fc8bb6fe4e94dde7b75
Harmen <harm@dds.nl>
2014-09-20 13:18:22 +0200

    First sorted set tests

---
+++ sorted_set_test.go
@@ -0,0 +1,47 @@
+package main
+
+// Sorted Set keys.
+
+import (
+	"testing"
+)
+
+func TestSortedSet(t *testing.T) {
+	testCommands(t,
+		succ("ZADD", "z", 1, "aap", 2, "noot", 3, "mies"),
+		succ("ZADD", "z", 1, "vuur", 4, "noot"),
+		succ("TYPE", "z"),
+		succ("EXISTS", "z"),
+		succ("ZCARD", "z"),
+
+		succ("ZRANK", "z", "aap"),
+		succ("ZRANK", "z", "noot"),
+		succ("ZRANK", "z", "mies"),
+		succ("ZRANK", "z", "vuur"),
+		succ("ZRANK", "z", "nosuch"),
+		succ("ZRANK", "nosuch", "nosuch"),
+
+		succ("ZADD", "zi", "inf", "aap", "-inf", "noot", "+inf", "mies"),
+		succ("ZRANK", "zi", "noot"),
+
+		// Double key
+		succ("ZADD", "zz", 1, "aap", 2, "aap"),
+		succ("ZCARD", "zz"),
+
+		// failure cases
+		succ("SET", "str", "I am a string"),
+		fail("ZADD"),
+		fail("ZADD", "s"),
+		fail("ZADD", "s", 1),
+		fail("ZADD", "s", 1, "aap", 1),
+		fail("ZADD", "s", "nofloat", "aap"),
+		fail("ZADD", "str", 1, "aap"),
+		fail("ZCARD"),
+		fail("ZCARD", "too", "many"),
+		fail("ZCARD", "str"),
+		fail("ZRANK"),
+		fail("ZRANK", "key"),
+		fail("ZRANK", "key", "too", "many"),
+		fail("ZRANK", "str", "member"),
+	)
+}
